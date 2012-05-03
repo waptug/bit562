4f7ad5a693dac80e17dac0dc1fef1db8770f5dc4
Mike McGinn <waptug@gmail.com>
2012-05-02 21:28:37 -0700

    Created some new tests.

--- tests/NexisgenMenuTestsMikeM.php
+++ tests/NexisgenMenuTestsMikeM.php
@@ -11,10 +11,28 @@ class TestMenuSystem extends WebTestCase {
         $this->clickSubmitById('submit');
         $this->assertCookie('PHPSESSID');
         $this->assertResponse(200);
-        $this->assertTitle('BIT562 - Home');
+        if($this->assertTitle('BIT562 - Home'))
+        {echo ('Successfully Loged in to the site. This test passed');}
+        else echo ('this test failed');
         $this->clickLink('Run jumpPointDoc');
         $this->assertResponse(200);
         $this->assertText('The jumpPoint run was successful.');
+        if($this->assertText('The jumpPoint run was successful.'))
+        {echo ('Successfully ran jumpPointdoc. This test passed');}
+        else echo ('this test failed');
+        $this->back();
+        //Try google
+        $this->clickLink('Search Google');//Trys to Open New Page that is secure https
+        $this->assertResponse(200);
+        $this->showRequest();
+        $this->showHeaders();
+        $this->showSource();
+        $this->showText();
+        $this->assertTitle('Google');
+        if($this->assertTitle('Google'))
+        {echo ('Successfully ran navagated back to home page and clicked on Search Google. This test passed');}
+        else echo ('this test failed');
+        // end
         $this->back();
         $this->clickLink('Output Docs');
         $this->assertResponse(200);
@@ -24,6 +42,7 @@ class TestMenuSystem extends WebTestCase {
         $this->back();
         $this->clickLink('not here');
         $this->assertText('text not here');
+        echo ('This test failed');
         //A test that will fail because it is a https page
         $this->clickLink('Free Project Hosting');//Trys to Open New Page that is secure https
         $this->showRequest();
@@ -32,7 +51,20 @@ class TestMenuSystem extends WebTestCase {
         $this->showText();
         $this->assertTitle('HostZilla, Inc. | Cloud Web Hosting Service Provider');
         $this->back();
-        $this->assertTitle('BIT562 - Home');
+        echo ('Click BACK button to return to provious page');
+        $this->showRequest();
+        $this->showHeaders();
+        $this->showSource();
+        $this->showText();
+        if($this->assertTitle('Login'))
+        {echo ('This test passed');}
+        else echo ('this test failed');
+        
+        $this->showRequest();
+        $this->showHeaders();
+        $this->showSource();
+        $this->showText();
+        $this->clickSubmitById('submit');
         $this->clickLink('Search Google');//Trys to Open New Page that is secure https
         $this->showRequest();
         $this->showHeaders();
@@ -41,6 +73,11 @@ class TestMenuSystem extends WebTestCase {
         $this->assertTitle('Google');
         $this->back();
         $this->assertTitle('BIT562 - Home');
+        //Test for a Joomla page
+        $url = 'http://www.joomla.org';
+        $this->get($url);
+        $this->showSource();
+        $this->assertText('<meta name="generator" content="Joomla!');
         
         
     }
